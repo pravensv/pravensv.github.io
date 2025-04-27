5ac513650961f37c437367af219da7f51f059146
Praveen Voruganti -X (prvoruga - ITALENT APPLICATIONS PRIVATE LIMITED at Cisco) <prvoruga@cisco.com>
2025-04-27 20:43:04 +0530

    test

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
       <div className={styles['about-text']}>
         <h1>About Me</h1>
         <p>
-          I’m <strong>Voruganti Praveen</strong>, a Java Full Stack Developer with 3+ years of experience in building scalable, responsive web applications using Java, Spring Boot, React.js, and more.
+          I’m <strong>Voruganti Praveen</strong>, a Java Full Stack Developer with 3.5+ years of experience in building scalable, responsive web applications using Java, Spring Boot, React.js, and more.
           I’m passionate about solving real-world problems through code and continuously improving my skills.
         </p>
       </div>
